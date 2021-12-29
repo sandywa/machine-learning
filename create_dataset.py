e69fc0e1ac9cbd71b42a250e63106a7c299594e7
Busroni <busroni10@gmail.com>
2021-12-29 23:34:24 +0700

    Update create_dataset.py

--- create_dataset.py
+++ create_dataset.py
@@ -1,7 +1,11 @@
 import cv2
 import numpy
-myimg = cv2.imread('image.jpg')
-avg_color_per_row = numpy.average(myimg, axis=0)
-avg_color = numpy.average(avg_color_per_row, axis=0)
-#color format is BGR not RGB, cv2 membaca gambar dalam format BGR.
-print(avg_color)
+from PIL import Image
+import glob
+image_list = []
+for filename in glob.glob('C:/Users/acer/Pictures/background/*.jpg'): #assuming jpg
+    myimg = cv2.imread(filename)
+    avg_color_per_row = numpy.average(myimg, axis=0)
+    avg_color = numpy.average(avg_color_per_row, axis=0)
+    #color format is BGR not RGB, cv2 membaca gambar dalam format BGR.
+    print(avg_color)
