4272d3f3a76b564baf5456ddde4b482cbc6a06d5
sandywa <sanwahyu34@gmail.com>
2021-12-31 10:56:59 +0700

    error in flask submit

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, send_from_directory
 import os
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = './static/uploads/'
 
 @app.route('/', methods=['GET' , 'POST'])
 def index():
@@ -10,7 +11,9 @@ def index():
 			image = request.files['image']
 			img_path = os.path.join(app.config['UPLOUD_FOLDER'],image.filename)
 			image.save(img_path)
-			return render_template('index.html?hasil='+img_path)
+			return render_template('index.html',fileDir=img_path)
+		else:
+			return render_template('hasil.html')
 
 	return render_template('index.html')
 
